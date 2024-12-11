2e742822a7cddbd0a9d1ee00f98674d743921383
Максим <maximers6e@gmail.com>
2024-12-11 10:35:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-vvod_ot_polzovatelya = int(input("Введите число: "))  # Запрос ввода числа у пользователя
-
-if vvod_ot_polzovatelya < 2:  # Проверка, если введенное число меньше 2 (такие числа не являются простыми)
-    print(f"{vvod_ot_polzovatelya} - не является простым числом")  # Вывод сообщения, что число не простое
-else:
-    is_prost = True  # Предполагаем, что число простое
-    for i in range(2, vvod_ot_polzovatelya):  # Проходим через все числа от 2 до введенного числа
-        if vvod_ot_polzovatelya % i == 0:  # Если найден делитель, то число не простое
-            is_prost = False
-            break
-    if is_prost:
-        print(f"{vvod_ot_polzovatelya} - простое число")  # Вывод сообщения, что число простое
-    else:
-        print(f"{vvod_ot_polzovatelya} - не является простым числом")  # Вывод сообщения, что число не простое
+n = int(input("vvedi:"))
+a = [i for i in range(n+1)]
+print (*a)
+a[1] = 0
+i = 2
+while 1 < n ** 0.5: 
+    if a[i] != 0:
+        j = i ** 2
+while j <= n:
+    a[j] = 0
+    j = j + i
+    i = i + 1
+    a = [i for i in a if a[i] !=0]
+    print (*a)
